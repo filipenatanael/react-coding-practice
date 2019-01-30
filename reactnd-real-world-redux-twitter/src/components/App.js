b061c74da7423cbd4266d364421e46d00ae55dfb
Filipe Natanael <filipenatanael@live.com>
2019-01-30 11:30:55 -0200

    feat: added Dashboard

--- reactnd-real-world-redux-twitter/src/components/App.js
+++ reactnd-real-world-redux-twitter/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
 
 class App extends Component {
   componentDidMount() {
@@ -9,14 +10,22 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        {
+          this.props.loading === true
+          ? null
+          : <Dashboard />
+        }
       </div>
     )
   }
 }
 
-// const mapStateToProps = (state) => {
-//
-// }
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
 
+
+// connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
 export default connect()(App)
