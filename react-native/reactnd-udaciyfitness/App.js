0efd53510e7c16df00778e74967d06c990827491
Filipe Natanael <filipenatanael@live.com>
2019-03-13 00:13:43 -0300

    feat: using platform-specific icons

--- react-native/reactnd-udaciyfitness/App.js
+++ react-native/reactnd-udaciyfitness/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AddEntry from './components/AddEntry'
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <AddEntry />
       </View>
     );
   }
@@ -14,7 +16,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
