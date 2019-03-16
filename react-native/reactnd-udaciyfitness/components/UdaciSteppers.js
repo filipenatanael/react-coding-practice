25a2d0a0b2a4d3fd76d3b1cb8d2cc1e302db5aa0
Filipe Natanael <filipenatanael@live.com>
2019-03-16 17:52:31 -0300

    feat: border Bottom Right Radius

--- react-native/reactnd-udaciyfitness/components/UdaciSteppers.js
+++ react-native/reactnd-udaciyfitness/components/UdaciSteppers.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native';
 import { FontAwesome, Entypo } from '@expo/vector-icons';
+import { purple, white } from '../utils/colors'
 
 export default function UdaciSteppers({ max, unit, step, value, onIncrement, onDecrement }) {
   return (
-    <View>
-      <View>
-        <TouchableOpacity onPress={onDecrement}>
-          <FontAwesome name='minus' size={30} color={'black'} />
+    <View style={[styles.row, { justifyContent: 'space-between' }]}>
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity
+          style={[styles.iosBtn, { borderTopRightRadius: 0, borderBottomRightRadius: 0 }]}
+          onPress={onDecrement}>
+            <FontAwesome name='minus' size={30} color={'black'} />
         </TouchableOpacity>
-        <TouchableOpacity onPress={onIncrement}>
-          <FontAwesome name='plus' size={30} color={'black'} />
+        <TouchableOpacity
+          style={[styles.iosBtn, { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }]}
+          onPress={onIncrement}>
+            <FontAwesome name='plus' size={30} color={'black'} />
         </TouchableOpacity>
       </View>
 
       <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+        <Text>{value}s</Text>
+        <Text>{unit}2</Text>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center'
+  },
+  iosBtn: {
+    backgroundColor: 'white',
+    borderColor: purple,
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 5,
+    paddingLeft: 25,
+    paddingRight: 25
+  }
+});
