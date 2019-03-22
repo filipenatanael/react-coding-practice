0f80d7a6d5f5cf0925a6fa2858ec15006a6ed379
Filipe Natanael <filipenatanael@live.com>
2019-03-21 22:09:32 -0300

    feat: create ButtonSubmit

--- react-styling-layout/react-components/src/App.js
+++ react-styling-layout/react-components/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormControl from './components/FormControl';
 import InputType from './components/InputType';
+import ButtonSubmit from './components/ButtonSubmit';
 
 class App extends Component {
   render() {
@@ -12,7 +13,9 @@ class App extends Component {
             name="firstName"
             placeholder="Enter your first name."
             required={true}
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
           />
+          <ButtonSubmit value="Sign Up" />
         </FormControl>
     );
   }
