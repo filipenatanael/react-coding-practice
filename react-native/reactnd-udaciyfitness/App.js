70b1ee6d20458122479bd511ae890bc72fe009db
Filipe Natanael <filipenatanael@live.com>
2019-03-24 19:08:45 -0300

    feat: UdacityStatusBar

--- react-native/reactnd-udaciyfitness/App.js
+++ react-native/reactnd-udaciyfitness/App.js
@@ -5,9 +5,18 @@ import { Provider } from 'react-redux';
 import { createMaterialTopTabNavigator, createAppContainer  } from 'react-navigation';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import reducer from './reducers';
+import { Constants } from 'expo';
 import AddEntry from './components/AddEntry';
 import History from './components/History';
 import { purple, white } from './utils/colors';
+
+function UdacityStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+     <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 const Tabs = createMaterialTopTabNavigator ({
   History: {
@@ -56,7 +65,7 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <StatusBar hidden />
+          <UdacityStatusBar backgroundColor={purple} barStyle="light-content" />
             <TabsContainer />
         </View>
       </Provider>
