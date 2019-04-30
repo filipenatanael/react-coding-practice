a116dc6f46919efd64351237c417b93f9a9932fe
Filipe Natanael <filipenatanael@live.com>
2019-04-29 23:24:34 -0300

    fix: adjusting uppercase function

--- my-own-plugins/react-create-component/helpers/index.js
+++ my-own-plugins/react-create-component/helpers/index.js
@@ -8,7 +8,7 @@ const titleize = (text) => {
   let words = text.toLowerCase().split(" ");
   for(let i = 0; i < words.length; i++) {
     let word = words[i];
-    words[i] = word.charAt(0).toUpperCase + word.slice(1);
+    words[i] = word.charAt(0).toUpperCase() + word.slice(1);
   }
   return words.join(" ");
 };
