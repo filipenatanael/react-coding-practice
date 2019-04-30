6c97900cab24058ec98519404d4f34465e0fdf7f
Filipe Natanael <filipenatanael@live.com>
2019-04-29 23:16:32 -0300

    test: lowercase first letters

--- my-own-plugins/react-create-component/tests/helpers/index.test.js
+++ my-own-plugins/react-create-component/tests/helpers/index.test.js
@@ -1,7 +1,15 @@
-const { capitalize } = require('../../helpers/index.js');
+const {
+  capitalize,
+  titleize
+} = require('../../helpers/index.js');
 
-test('uppercase first lettter.', () => {
+test('uppercase first letter.', () => {
   let receivedvalue = capitalize('title');
   let expectedvalue = 'Title';
   expect(receivedvalue).toBe(expectedvalue);
+});
+
+test('lowercase first letters.', () => {
+  let text = "Hello world!";
+  expect(titleize(text)).toBe('Hello World!');
 });
