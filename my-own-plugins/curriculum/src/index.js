c941c0d51a6417e47b37b837c1576675ed1743cd
Filipe Natanael <filipenatanael@live.com>
2019-05-01 21:48:58 -0300

    refactor: removed unnecessaries files

--- my-own-plugins/curriculum/src/index.js
+++ my-own-plugins/curriculum/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './assets/plugins/bootstrap/css/bootstrap.min.css';
-import './index.css';
+import './assets/css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
