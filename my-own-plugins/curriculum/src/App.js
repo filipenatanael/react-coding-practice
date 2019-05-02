dfbfb3aa3d993ba18994c39e59a76d01a6b2f984
Filipe Natanael <filipenatanael@live.com>
2019-05-01 22:31:58 -0300

    feat: up to date scss styling

--- my-own-plugins/curriculum/src/App.js
+++ my-own-plugins/curriculum/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
-import './assets/css/App.css';
+
 
 function App() {
   return (
--- my-own-plugins/curriculum/src/index.js
+++ my-own-plugins/curriculum/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/plugins/bootstrap/css/bootstrap.min.css';
-import './assets/css/index.css';
+import './assets/scss/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
