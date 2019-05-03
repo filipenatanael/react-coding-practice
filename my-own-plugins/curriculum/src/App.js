d1280d46e08c5c8cf8438f8b20b60b40634dc4ba
Filipe Natanael <filipenatanael@live.com>
2019-05-02 23:38:45 -0300

    refactor: added profile to component

--- my-own-plugins/curriculum/src/App.js
+++ my-own-plugins/curriculum/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import Profile from './components/Profile';
 
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
          <div className="sidebar-wrapper">
-             <div className="profile-container">
-                 <img className="profile" src="https://themes.3rdwavemedia.com/demo/orbit/assets/images/profile.png" alt="" />
-                 <h1 className="name">Alan Doe</h1>
-                 <h3 className="tagline">Full Stack Developer</h3>
-             </div>
+            <Profile />
 
              <div className="contact-container container-block">
                  <ul className="list-unstyled contact-list">
